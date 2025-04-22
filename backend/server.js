ad8d96ed3fe84a861b3878d979e5fddc6d39560b
Nadeem_khan <nk8452868@gmail.com>
2025-04-22 14:24:27 +0530

    server error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import passport from "passport";
 import session from "express-session";
 import path from "path";
-import { fileURLToPath } from "url";
+
 import "./passport/github.auth.js";
 
 import userRoutes from "./routes/user.route.js";
@@ -17,32 +17,26 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
-
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
+// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Static file serving
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-
-
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
